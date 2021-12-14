89f304f8ad28a2abb36934585843ef02d3280db6
AzureAD\EdwinPatricioZamoraM <ezamormo@everis.com>
2021-12-14 11:33:54 -0500

    springWbeFlux mogobd

---
+++ TDDSpringJUnit5/reactivemongodb/src/main/java/com/reactive/mongodb/reactivemongodb/ReactivemongodbApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.mongodb.reactivemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivemongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivemongodbApplication.class, args);
+	}
+
+}
