9888b975729db0493e95d76100fee3ba7d914fb1
edzamo13@gmail.com <ezamora@mag.gob.ec>
2022-07-26 17:03:54 -0500

    add controller and log of apacheCommon

--- springBasic/curso-introduccion-platzi/src/main/java/com/platzi/springboot/controller/Test.java
+++ springBasic/curso-introduccion-platzi/src/main/java/com/platzi/springboot/controller/Test.java
@@ -1,4 +1,27 @@
 package com.platzi.springboot.controller;
 
+import com.platzi.springboot.configuration.GeneralConfiguration;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class Test {
+
+    @Autowired
+    private GeneralConfiguration configuration;
+
+    Log LOOGGER= LogFactory.getLog(Test.class);
+
+    @GetMapping
+    public ResponseEntity<String> getName() {
+        LOOGGER.info("into the class");
+        return new ResponseEntity<>("hola mi nonbre es:" + configuration.getName() + " y mi apellido " + configuration.getLastName(), HttpStatus.OK);
+    }
 }
