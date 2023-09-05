aec0ae2c18c1fad159d729df603130b5c6faac3d
edzamo13 <edzamo13@gmail.com>
2023-09-05 11:44:09 -0500

    add function endPoint

--- springWebFlux/webflux-demo/src/main/java/com/ezamora/webfluxdemo/config/RouterConfig.java
+++ springWebFlux/webflux-demo/src/main/java/com/ezamora/webfluxdemo/config/RouterConfig.java
@@ -6,6 +6,8 @@ import java.util.function.BiFunction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -26,17 +28,20 @@ public class RouterConfig {
         .build();
   }
 
-//  @Bean
+  //  @Bean
   public RouterFunction<ServerResponse> serverResponseRouterFunction() {
     return RouterFunctions
         .route()
-       // .GET("router/square/{input}", requestHandler::squareHandler)
-        .GET("router/square/{input}", requestHandler::squareHandler)
-        .GET("router/square/{input}", requestHandler::squareHandler)
-        .GET("router/table/{input}", requestHandler::tableHandler)
-        .GET("router/table/{input}/stream", requestHandler::tableStreamHandler)
-        .POST("router/multiply", requestHandler::multiplyHandler)
-        .GET("router/square/{input}/validation", requestHandler::squareHandlerWithValidation)
+        // .GET("router/square/{input}", requestHandler::squareHandler)
+        .GET("square/{input}", RequestPredicates.path("*/1?").or(RequestPredicates.path("*/20")),
+            requestHandler::squareHandler)
+        .GET("square/{input}",
+            request -> ServerResponse.badRequest().bodyValue("only 10-19 allowed "))
+        .GET("square/{input}", requestHandler::squareHandler)
+        .GET("table/{input}", requestHandler::tableHandler)
+        .GET("table/{input}/stream", requestHandler::tableStreamHandler)
+        .POST("multiply", requestHandler::multiplyHandler)
+        .GET("square/{input}/validation", requestHandler::squareHandlerWithValidation)
         .onError(InputValidationException.class, exceptionHandler())
         .build();
   }
